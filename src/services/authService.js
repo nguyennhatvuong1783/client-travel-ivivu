822ab5c2dc1dac76ef8a619cc28726de06c9545c
Vuong <nhatvuong178@gmail.com>
2024-11-24 12:58:59 +0700

    fix: call api myInfo

--- src/services/authService.js
+++ src/services/authService.js
@@ -13,9 +13,5 @@ export const register = (registerData) => {
 };
 
 export const myInfo = () => {
-    return axiosInstance.get('/users', {
-        headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W3siYXV0aG9yaXR5IjoiQURNSU4ifV0sInN1YiI6ImFkbWluIiwiaWF0IjoxNzMyMzUwNjgyLCJleHAiOjE3MzIzNTQyODIsImp0aSI6IjcxZjYxNjRiLWI1MWYtNGJjYy1iYzk3LTdiMmNkNjUzYzE3OSJ9.JyTXHf-x04mCSVt1OSc_3gPmRe2JxMTdVyRVwg2M6qpjRGHV9Hc3Nv9zkBge2FwftiYrVM-KIqp0x4l22l0p8A`,
-        }
-    });
+    return axiosInstance.get('/myInfo');
 };  
